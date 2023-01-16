18cb868b8586a9016f273fb055c0cc28ca1030cb
Valihan <valihanburhanov@yandex.ru>
2023-01-16 10:30:45 +0300

    Task1 (complete)

--- c#homework2.cs
+++ c#homework2.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на 
+// вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    Console.WriteLine($"Вторая цифра числа {number} -> {(number%100)/10}");
+}
+
+else
+{
+    Console.WriteLine("Неправильное число!");
+}
