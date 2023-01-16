5eaff2ee2c290f244d6a34ad7412bb8603f80575
Valihan <valihanburhanov@yandex.ru>
2023-01-16 11:47:23 +0300

    Task3 (complete)

--- c#homework2.cs
+++ c#homework2.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите цифру, обозначающую день недели :");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a==6 || a==7)
+{
+    Console.Write("Да, это выходной");
+}
+else
+{
+    Console.Write("Это не выходной");
+}
